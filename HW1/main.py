a7a83c105073d882a174379a261dd1e9010bd3fd
Jeremy <jeremy.l.griffith@gmail.com>
2022-09-11 16:53:37 -0500

    Connection code from lesson 09/10

--- HW1/main.py
+++ HW1/main.py
@@ -1,8 +1,19 @@
-#this is HW1
+# this is HW1
 
 import mysql.connector
 from mysql.connector import Error
 
+
 def create_con(hostname, username, userpw, dbname):
-
-
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=hostname,
+            user=username,
+            password=userpw,
+            database=dbname
+        )
+        print("success")
+    except Error as e:
+        print(f'the error {e} occurred')
+    return connection
