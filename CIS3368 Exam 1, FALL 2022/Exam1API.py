ed53c5859bb08fa4a6a9396f04b977b7f0108156
Jeremy <jeremy.l.griffith@gmail.com>
2022-10-08 12:04:44 -0500

    Initial API done, need to get token req done for DELETE

---
+++ CIS3368 Exam 1, FALL 2022/Exam1API.py
@@ -0,0 +1,111 @@
+############
+# MYSQL CODE
+############
+
+# create table watches (
+# id int NOT NULL auto_increment,
+# make varchar(25) NOT NULL,
+# model varchar(25) NOT NULL,
+# type varchar(25) NOT NULL,
+# purchaseprice decimal(13,2) NOT NULL,
+# salesprice decimal(13,2) NOT NULL,
+# PRIMARY KEY (id)
+# )
+
+# INSERT EXAMPLE
+# insert into watches (make, model, type, purchaseprice, salesprice)
+# VALUES ('cartier', 'santos-dumont', 'quartz', 3200.00, 4000.00);
+
+import flask
+from flask import jsonify
+from flask import request, flash
+from sql import create_connection
+from sql import execute_read_query
+import creds
+
+###########
+# APIs
+###########
+
+# setting up app name
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1> If you see this, they will grade </h1>"
+
+
+# GET
+@app.route('/api/watch', methods=['GET'])
+def watch_all():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    sql = "SELECT * FROM watches"
+    watches = execute_read_query(conn, sql)
+    return jsonify(watches)
+
+
+# PUT
+@app.route('/api/watch', methods=['PUT'])
+def update_watch():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    cursor = conn.cursor()
+    request_data = request.get_json()
+    change_id = request_data['id']
+    change_make = request_data['make']
+    change_model = request_data['model']
+    change_type = request_data['type']
+    change_purchase_price = request_data['purchaseprice']
+    change_sales_price = request_data['salesprice']
+    sql = "UPDATE watches SET make=%s, model=%s, type=%s, purchaseprice=%s, salesprice=%s WHERE id=%s"
+    data = (change_make, change_model, change_type, change_purchase_price, change_sales_price, change_id)
+    cursor.execute(sql, data)
+    conn.commit()
+    sql = "SELECT * FROM watches"
+    watches = execute_read_query(conn, sql)
+    return jsonify(watches)
+
+
+# POST
+@app.route('/api/watch', methods=['POST'])
+def add_watch():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    cursor = conn.cursor()
+    request_data = request.get_json()
+    add_make = request_data['make']
+    add_model = request_data['model']
+    add_type = request_data['type']
+    add_purchase_price = request_data['purchaseprice']
+    add_sales_price = request_data['salesprice']
+    sql = "INSERT INTO watches (make, model, type, purchaseprice, salesprice) VALUES (%s, %s, %s, %s, %s)"
+    data = (add_make, add_model, add_type, add_purchase_price, add_sales_price)
+    cursor.execute(sql, data)
+    conn.commit()
+    sql = "SELECT * FROM watches"
+    watches = execute_read_query(conn, sql)
+    return jsonify(watches)
+
+
+# DELETE
+@app.route('/api/watch', methods=['DELETE'])
+def del_watch():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    cursor = conn.cursor()
+    request_data = request.get_json()
+    del_id = request_data['id']
+    sql = "DELETE FROM watches WHERE id=%s"
+    data = (del_id,)
+    cursor.execute(sql, data)
+    conn.commit()
+    sql = "SELECT * FROM watches"
+    watches = execute_read_query(conn, sql)
+    return jsonify(watches)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
+
---
+++ Class Notes/classnotes100122hash.py
@@ -0,0 +1,88 @@
+import hashlib
+import datetime
+import time
+import flask
+from flask import jsonify
+from flask import request, make_response
+
+# 5316911983139663491615228241121400000 GUID/UUID
+# 80658175170943878571660636856403766975289505440883277824000000000000 = 52!
+# 115792089237316195423570985008687907853269984665640564039457584007913129639936 = 2^256, SHA256
+
+
+# setting up an application name
+app = flask.Flask(__name__)  # sets up the application
+app.config["DEBUG"] = True  # allow to show errors in browser
+
+# password 'password' hashed
+master_password = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
+master_name = 'username'
+valid_tokens = {
+    '100', '200', '300', '400'
+}
+
+
+# basic http authentication, prompts username and password upon contacting the endpoint
+@app.route('/authenticatedroute', methods=['GET'])
+def auth_example():
+    if request.authorization:
+        encoded = request.authorization.password.encode()  # unicode encoding
+        hashed_result = hashlib.sha256(encoded)
+        if request.authorization.username == master_name and hashed_result.hexdigest() == master_password:
+            return '<h1> WE ARE ALLOWED TO BE HERE! </h1>'
+    return make_response('COULD NOT VERIFY!', 401, {'WWW-Authenticate': 'Basic realm = "Login Required"'})
+
+
+# token submission as part of the url, similar to how personal tokens are part of SMS auth.
+@app.route('/api/token/<token>', methods=['GET'])
+def auth_token(token):
+    for valid_token in valid_tokens:
+        if token == valid_token:  # check if token is valid, compared against a set of valid token in a db table
+            return '<h1> CONGRATULATIONS, AUTH Successful </h1>'
+    return 'INVALID ACCESS TOKEN'
+
+
+# tokens that have an expiration dat and time, is given if the token is not expired
+# for instance, time token valid until Jan 1, 2030 (still valid): 1893456000
+# for instance, time token valid until Jan 2, 2020 (no longer valid): 1640995200
+# date = datetime.datetime(2022,1,1)
+@app.route('/api/timetoken/<timetoken>', methods=['GET'])
+def auth_timetoken(timetoken):
+    if float(timetoken) > time.time():  # checks if the token is greater than current date
+        return '<h1> YOUR TIME TOKEN IS VALID </h1>'
+    return '<h1> YOUR TIME TOKEN HAS EXPIRED</h1>'
+
+
+authorized_users = [
+    {
+        'username': 'username',
+        'password': 'password',
+        'role': 'default',
+        'token': '0',
+        'admininfo': None
+    },
+    {
+        'username': 'jeremy',
+        'password': 'admin',
+        'role': 'admin',
+        'token': '1234567890',
+        'admininfo': 'something applicable to admin'
+    }
+]
+
+
+# route to auth with username and password against a dataset (ideally from DB w/ password hashed)
+@app.route('/api/usernamepw', methods=['GET'])
+def usernamepw_auth():
+    username = request.headers['username']  # get the header parameters (as dictionaries)
+    password = request.headers['password']
+    for au in authorized_users:
+        if au['username'] == username and au['password'] == password:
+            session_token = au['token']
+            admin_info = au['adminifo']
+            return_info = [au['role'], session_token, admin_info]
+            return jsonify(return_info)
+    return 'SECURITY ERROR'  # never give details on what went wrong
+
+
+app.run()
