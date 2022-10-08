76c46fb53e86371701f1a374ade0b558e8e1c80c
Jeremy <jeremy.l.griffith@gmail.com>
2022-10-08 13:57:04 -0500

    extra credit attempt, I figured out how to use binary as the datatype for the ID attribute, and then how to insert using UUID, but I had problems figuring out how to display the results using JSONIFY.  It was giving me following error: "TypeError: Object of type bytearray is not JSON serializable" I did put examples of the MYSQL table to use UUID, and found some documentation on how to create UUID in python.

--- CIS3368 Exam 1, FALL 2022/Exam1API.py
+++ CIS3368 Exam 1, FALL 2022/Exam1API.py
@@ -2,7 +2,15 @@
 # MYSQL CODE
 ############
 
-# create table watches (
+# two tables
+# watches = extra credit using UUID
+# watches1 = auto-increment INT
+
+###################
+# watches1 db table
+###################
+
+# create table watches1 (
 # id int NOT NULL auto_increment,
 # make varchar(25) NOT NULL,
 # model varchar(25) NOT NULL,
@@ -13,19 +21,52 @@
 # )
 
 # INSERT EXAMPLE
-# insert into watches (make, model, type, purchaseprice, salesprice)
+# insert into watches1 (make, model, type, purchaseprice, salesprice)
 # VALUES ('cartier', 'santos-dumont', 'quartz', 3200.00, 4000.00);
 
+##################
+# watches db table
+##################
+
+# create table watches (
+# id binary(16) PRIMARY KEY,
+# make varchar(25) NOT NULL,
+# model varchar(25) NOT NULL,
+# type varchar(25) NOT NULL,
+# purchaseprice decimal(13,2) NOT NULL,
+# salesprice decimal(13,2) NOT NULL
+# );
+
+# INSERT EXAMPLE
+# insert into watches(id, make, model, type, purchaseprice, salesprice)
+# values (UUID_TO_BIN(UUID()), 'rolex', 'oyster', 'mechanical', 8000.00, 10000.00),
+# (UUID_TO_BIN(UUID()), 'cartier', 'large', 'mechanical', 5800.00, 6800.00),
+# (UUID_TO_BIN(UUID()), 'br-x1', 'skeleton', 'mechanical', 400000.00, 480000.00),
+# (UUID_TO_BIN(UUID()), 'garmin', 'fenix7', 'solar', 800.00, 899.99),
+# (UUID_TO_BIN(UUID()), 'cartier', 'santos-dumont', 'quartz', 3200.00, 4000.00);
+
+
+###########
+# modules
+###########
+
+
 import flask
+import json
+import uuid
+from json import JSONEncoder
+from uuid import UUID
 from flask import jsonify
 from flask import request, flash
 from sql import create_connection
 from sql import execute_read_query
 import creds
 
+
 ###########
 # APIs
 ###########
+
 
 # setting up app name
 app = flask.Flask(__name__)
@@ -45,14 +86,92 @@ def home():
 def watch_all():
     my_creds = creds.Creds()
     conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    sql = "SELECT * FROM watches1"
+    watches1 = execute_read_query(conn, sql)
+    return jsonify(watches1)
+
+
+# PUT
+@app.route('/api/watch', methods=['PUT'])
+def update_watch():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    cursor = conn.cursor()
+    request_data = request.get_json()
+    change_id = request_data['id']
+    change_make = request_data['make']
+    change_model = request_data['model']
+    change_type = request_data['type']
+    change_purchase_price = request_data['purchaseprice']
+    change_sales_price = request_data['salesprice']
+    sql = "UPDATE watches1 SET make=%s, model=%s, type=%s, purchaseprice=%s, salesprice=%s WHERE id=%s"
+    data = (change_make, change_model, change_type, change_purchase_price, change_sales_price, change_id)
+    cursor.execute(sql, data)
+    conn.commit()
+    sql = "SELECT * FROM watches1"
+    watches1 = execute_read_query(conn, sql)
+    return jsonify(watches1)
+
+
+# POST
+@app.route('/api/watch', methods=['POST'])
+def add_watch():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+    cursor = conn.cursor()
+    request_data = request.get_json()
+    add_make = request_data['make']
+    add_model = request_data['model']
+    add_type = request_data['type']
+    add_purchase_price = request_data['purchaseprice']
+    add_sales_price = request_data['salesprice']
+    sql = "INSERT INTO watches1 (make, model, type, purchaseprice, salesprice) VALUES (%s, %s, %s, %s, %s)"
+    data = (add_make, add_model, add_type, add_purchase_price, add_sales_price)
+    cursor.execute(sql, data)
+    conn.commit()
+    sql = "SELECT * FROM watches1"
+    watches1 = execute_read_query(conn, sql)
+    return jsonify(watches1)
+
+
+# DELETE
+@app.route('/api/watch/token/<token>', methods=['DELETE'])
+def del_watch(token):
+    for valid_token in valid_tokens:
+        if token == valid_token:
+            my_creds = creds.Creds()
+            conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
+            cursor = conn.cursor()
+            request_data = request.get_json()
+            del_id = request_data['id']
+            sql = "DELETE FROM watches1 WHERE id=%s"
+            data = (del_id,)
+            cursor.execute(sql, data)
+            conn.commit()
+            sql = "SELECT * FROM watches1"
+            watches1 = execute_read_query(conn, sql)
+            return jsonify(watches1)
+    return 'INVALID ACCESS TOKEN'
+
+
+##############
+# EXTRA CREDIT
+##############
+
+
+# GET
+@app.route('/api/watch/xtra', methods=['GET'])
+def xtra_watch_all():
+    my_creds = creds.Creds()
+    conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
     sql = "SELECT * FROM watches"
     watches = execute_read_query(conn, sql)
     return jsonify(watches)
 
 
 # PUT
-@app.route('/api/watch', methods=['PUT'])
-def update_watch():
+@app.route('/api/watch/xtra', methods=['PUT'])
+def xtra_update_watch():
     my_creds = creds.Creds()
     conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
     cursor = conn.cursor()
@@ -73,8 +192,8 @@ def update_watch():
 
 
 # POST
-@app.route('/api/watch', methods=['POST'])
-def add_watch():
+@app.route('/api/watch/xtra', methods=['POST'])
+def xtra_add_watch():
     my_creds = creds.Creds()
     conn = create_connection(my_creds.conString, my_creds.userName, my_creds.password, my_creds.dbName)
     cursor = conn.cursor()
@@ -84,7 +203,7 @@ def add_watch():
     add_type = request_data['type']
     add_purchase_price = request_data['purchaseprice']
     add_sales_price = request_data['salesprice']
-    sql = "INSERT INTO watches (make, model, type, purchaseprice, salesprice) VALUES (%s, %s, %s, %s, %s)"
+    sql = "INSERT INTO watches (make, model, type, purchaseprice, salesprice) VALUES (UUID_TO_BIN(UUID()), %s, %s, %s, %s, %s)"
     data = (add_make, add_model, add_type, add_purchase_price, add_sales_price)
     cursor.execute(sql, data)
     conn.commit()
@@ -94,8 +213,8 @@ def add_watch():
 
 
 # DELETE
-@app.route('/api/watch/token/<token>', methods=['DELETE'])
-def del_watch(token):
+@app.route('/api/watch/xtra/token/<token>', methods=['DELETE'])
+def xtra_del_watch(token):
     for valid_token in valid_tokens:
         if token == valid_token:
             my_creds = creds.Creds()
@@ -107,7 +226,7 @@ def del_watch(token):
             data = (del_id,)
             cursor.execute(sql, data)
             conn.commit()
-            sql = "SELECT * FROM watches"
+            sql = "SELECT * FROM watches1"
             watches = execute_read_query(conn, sql)
             return jsonify(watches)
     return 'INVALID ACCESS TOKEN'
